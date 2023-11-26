7e7b78b5585f9fa48a1081d19ae184d91dc61f0f
brudaninDanil <gasink@mail.ru>
2023-11-27 00:16:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x, y, a, b;
+
+        Console.WriteLine("Введите x, a и b:");
+        string[] input = Console.ReadLine().Split(' ');
+        x = Convert.ToDouble(input[0]);
+        a = Convert.ToDouble(input[1]);
+        b = Convert.ToDouble(input[2]);
+
+        if (a < b)
+        {
+            y = a / b + 1;
+        }
+        else if (a == b)
+        {
+            y = -1;
+        }
+        else
+        {
+            y = (a * b - 5) / a;
+        }
+
+        Console.WriteLine($"x=  {y.ToString("0.0")}");
+    }
+}
